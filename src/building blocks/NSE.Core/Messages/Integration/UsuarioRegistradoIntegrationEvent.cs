8f429546bdf96fe100321ef6e7e8abea6d6b54f3
woto <wotoss10@gmail.com>
2021-09-12 23:47:08 -0300

    Intalação Docker e portainer.io e inicio de implementação comunicação entre API Request e Response baseado em fila

---
+++ src/building blocks/NSE.Core/Messages/Integration/UsuarioRegistradoIntegrationEvent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NSE.Core.Messages.Integration
+{
+    //esta classe está no passado o usuário já foi registrado
+    //Lembrando que esta é a mensagem que será o meu request ela (vai) vou criar uma classe que volta msg será ResponseMessage
+    public class UsuarioRegistradoIntegrationEvent : IntegrationEvent
+    {
+        //quando eu tenho o set privado eu uso construtor
+        public Guid Id { get; private set; }
+
+        public string Nome { get; private set; }
+
+        public string Email { get; private set; }
+
+        public string Cpf { get; private set; }
+
+        public UsuarioRegistradoIntegrationEvent(Guid id, string nome, string email, string cpf)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+        }
+    }
+}
